56a04a5fbbfc6c532d04f40f97c239c27f8638ed
kraken001k <s.s.zeibel@student.khai.edu>
2025-12-14 01:13:54 +0200

    feat: PR12 jQuery burger, language toggle, slick news slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log("Сайт кафедри завантажено успішно!");
+$(function () {
+    console.log("Сайт кафедри завантажено успішно!");
+
+    // Бургер-меню
+    $('.menu-toggle').on('click', function () {
+        $('.main-menu').toggleClass('show');
+    });
+
+    // Перемикач мови
+    $('.lang-wrapper a').on('click', function (e) {
+        e.preventDefault();
+        $('.lang-wrapper a').removeClass('is-active');
+        $(this).addClass('is-active');
+    });
+
+    // Slick Slider для новин
+    const $slickElement = $('#news .news-cards');
+
+    $slickElement.not('.slick-initialized').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0',
+        cssEase: 'ease-in-out',
+        responsive: [
+            { breakpoint: 1200, settings: { slidesToShow: 4 } },
+            { breakpoint: 992,  settings: { slidesToShow: 2 } },
+            { breakpoint: 576,  settings: { slidesToShow: 1 } }
+        ]
+    });
+});
